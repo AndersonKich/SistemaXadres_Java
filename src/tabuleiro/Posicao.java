53627cc1ee8fc754fc9be6576fc8dd972afb6bd0
Anderson Kich <kich24@hotmail.com>
2019-07-03 05:53:17 -0300

    Clase posicao criada

---
+++ src/tabuleiro/Posicao.java
@@ -0,0 +1,47 @@
+package tabuleiro;
+
+public class Posicao {
+
+	private Integer linha; //row
+	private Integer coluna; //column
+	
+	
+	public Posicao() {
+		
+	}
+	public Posicao(Integer linha, Integer coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	public Integer getLinha() {
+		return linha;
+	}
+	public void setLinha(Integer linha) {
+		this.linha = linha;
+	}
+	public Integer getColuna() {
+		return coluna;
+	}
+	public void setColuna(Integer coluna) {
+		this.coluna = coluna;
+	}
+	
+	public void definirValores(int linha, int coluna) {
+		
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "Linha: "+linha+", Coluna: "+coluna;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
